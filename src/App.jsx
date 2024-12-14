be2724447adbd25451f712c64103f4d7d93e409c
Priyanshu123 <prince678p@gmail.com>
2024-12-14 14:42:10 +0530

    add the web name

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ function App() {
     <section className="container">
       <div className="clock_content">
         <h1>We are almost there</h1>
-        <h3 className="title">Stay tuned for something amazing.</h3>
+        <h3 className="title">
+          Stay tuned for something amazing with New With Evidence.
+        </h3>
         <FlipClockCountdown
           className="flip-clock"
           to={new Date().getTime() + 30 * 24 * 3600 * 1000}
