6118b164bc3f1721360d1b9ef7ce60becd5a5ad7
Priyanshu123 <prince678p@gmail.com>
2024-12-14 13:49:02 +0530

    comming soon page

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
+import "@leenguyen/react-flip-clock-countdown/dist/index.css";
+import "./App.css";
+
+function App() {
+  return (
+    <section className="container">
+      <div className="clock_content">
+        <h1>We are almost there</h1>
+        <h3 className="title">Stay tuned for something amazing.</h3>
+        <FlipClockCountdown
+          className="flip-clock"
+          to={new Date().getTime() + 24 * 3600 * 1000 + 5000}
+          labels={["DAYS", "HOURS", "MINUTES", "SECONDS"]}
+          duration={0.5}
+        />
+        <button className="button">Notify Me</button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
