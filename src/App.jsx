5cc5ad712106831034c41e8462de778b93fa6a8f
Priyanshu123 <prince678p@gmail.com>
2025-07-10 09:03:15 +0530

    add the UjI

--- src/App.jsx
+++ src/App.jsx
@@ -53,27 +53,26 @@ const paymentCards = [
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-[#E8E4FF] flex flex-col">
+    <div className="min-h-screen w-screen bg-[#E8E4FF] flex flex-col">
       <Navigation />
-      <main className="flex-1 w-full max-w-7xl mx-auto px-6 py-4 bg-[#F6F7FB] rounded-b-2xl">
+      <main className="flex-1 w-full max-w-7xl mx-auto px-6 py-4 bg-[#F6F7FB] rounded-b-2xl flex flex-col">
         <Header />
-        <div className="mt-6 grid grid-cols-1 md:grid-cols-10 gap-6">
-          <div className="md:col-span-3 col-span-1">
-            <div className="flex flex-col gap-3">
-              <UpcomingJobs />
-              <JobsAssigned />
-            </div>
+        <div className="mt-6 grid grid-cols-1 md:grid-cols-10 gap-6 items-stretch flex-1">
+          {/* Upcoming Jobs & Jobs Assigned */}
+          <div className="md:col-span-3 col-span-1 flex flex-col gap-3 h-full">
+            <UpcomingJobs />
+            <JobsAssigned />
           </div>
-
-          <div className="md:col-span-3 col-span-1 flex flex-col gap-6">
+          {/* Today's Stats & Invoice Summary */}
+          <div className="md:col-span-3 col-span-1 flex flex-col gap-6 h-full">
             <TodaysStats />
             <InvoiceSummary
               invoiceData={invoiceData}
               invoiceStats={invoiceStats}
             />
           </div>
-
-          <div className="md:col-span-4 col-span-1 flex flex-col gap-6">
+          {/* Incoming Jobs & Payment Cards */}
+          <div className="md:col-span-4 col-span-1 flex flex-col gap-6 h-full">
             <IncomingJobs />
             <PaymentCards paymentCards={paymentCards} />
           </div>
